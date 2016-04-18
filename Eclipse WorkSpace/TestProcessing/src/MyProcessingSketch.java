764048724eb13765da1da1dad6ebd7a487735f93
Alex <alex.witte@hotmail.com>
2016-04-18 19:15:44 +0100

    Updates

--- Eclipse WorkSpace/TestProcessing/src/MyProcessingSketch.java
+++ Eclipse WorkSpace/TestProcessing/src/MyProcessingSketch.java
@@ -1,20 +1,62 @@
 import processing.core.*;
+import ddf.minim.*;
+import controlP5.*;
 
-public class MyProcessingSketch extends PApplet {
 
-  public void setup() {
-    size(600,600);
-    background(0);
-  }
+
+public class MyProcessingSketch extends PApplet 
+{
+	Minim m;
+	AudioPlayer song;
+	
+	ControlP5 cp5;
+	
+	int mode = 1;
+	
+	
+	
+	public void setup() 
+	{
+	    size(1200,1200);
+	    background(0);
+	    
+	    m = new Minim(this);
+	    //loads song from the data folder
+	    song = m.loadFile("Drop It Like It's Hot.mp3");
+	    song.play();
+	}//end setup
   
-  public static void main(String args[]) {
+	
+	
+  public static void main(String args[]) 
+  {
 	    PApplet.main(new String[] { "--present", "MyProcessingSketch" });
-	  }
+  }//end main
 
-  public void draw() {
+  
+  
+  public void draw() 
+  {
+	  switch(mode)
+	  {
+		  case 1:
+		  {
+		  }
+	  
+		  case 2:
+		  {
+		  }
+	  
+		  case 3:
+		  {
+		  }
+	  }//end switch
+	  
     stroke(255);
-    if (mousePressed) {
+    if (mousePressed) 
+    {
+    	
       line(mouseX,mouseY,pmouseX,pmouseY);
-    }
-  }
-}
+    }//end mousePressed
+  }//end draw
+}//end Sketch
